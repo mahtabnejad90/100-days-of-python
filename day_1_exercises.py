45f55ead64d2016e13b5c13568e727a1bd387e98
Mahtab Nejad <mahtab@bahmannejad.com>
2022-01-26 23:06:50 +0000

    day 1 code snippets

---
+++ day_1_exercises.py
@@ -0,0 +1,84 @@
+# Write your code below this line
+
+print("Hello world!\nHello world!\nHello World!")
+
+# Day 1 - Exercise 1 - Printing
+
+print('Day 1 - Python Print Function')
+
+print("The function is declared like this:")
+
+print("print('what to print')")
+
+print("Hello" + " " + "Mahtab")
+
+# Day 1 - Exercise 2 - Debugging
+
+# Fix the code below (Code fixed)
+# Indentation is important in python
+
+print("Day 1 - String Manipulation")
+
+print("String Concatenation is done with the" + ' "+" ' + "sign.")
+
+print('e.g. print("Hello " + "world")')
+
+print("New lines can be created with a backslash and n.")
+
+# Input function - a prompt for the user
+
+print("hello " + input("What is your name?"))
+
+# Day 1 - Exercise 3 - Input
+
+# prints out the length of the input
+
+print(len(input("What is your name?")))
+
+# variables
+
+name1 = input("What is your name?")
+
+print(name1)
+
+name2 = "Mahtab1"
+print(name2)
+name2 = "Mahtab2"
+print(name2)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+# Day 1 - Exercise 4 - Variables - Write a program that switches the values stored in the variables a and b.
+
+# Don't change the code below
+a = input("a: ")
+b = input("b: ")
+# Don't change the code above
+
+####################################
+#Write your code below this line
+
+c = a
+a = b
+b = c
+
+# Write your code above this line
+####################################
+
+# Don't change the code below
+print("a: " + a)
+print("b: " + b)
+
+# Conventions
+
+# Make it readable and meaningful
+
+user_name # _ to seperate words
+
+# cannot use spaces to split two words 
+
+# variables cannot start with numbers, will generate syntax error
+
+# cannot use privilage names to avoid syntax errors, conflicts or confusion
---
+++ day_1_project.py
@@ -0,0 +1,27 @@
+#1. Create a greeting for your program.
+
+print("What is your name")
+
+name = input()
+
+print("Hello " + name + ", Welcome to the band generator")
+
+#2. Ask the user for the city that they grew up in.
+
+print("Which city did you grow up in?")
+
+city = input()
+
+#3. Ask the user for the name of a pet.
+
+print("Whats the name of a pet?")
+
+pet = input()
+
+#4. Combine the name of their city and pet and show them their band name.
+
+band_name = city + " " + pet
+
+print("Your band name is called " + band_name)
+
+#5. Make sure the input cursor shows on a new line
